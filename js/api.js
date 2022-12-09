026ec4fb4b73060442932e09a9f8304fb7354cdd
ssuyun.chen <ssuyun.chen@udngroup.com>
2022-12-09 11:58:44 +0800

    feat: 調整API

--- js/api.js
+++ js/api.js
@@ -1,16 +1,16 @@
-const baseRequest = axios.create({
-  baseURL: 'https://livejs-api.hexschool.io/api/livejs/v1/'
+const customerRequest = axios.create({
+  baseURL: `https://livejs-api.hexschool.io/api/livejs/v1/customer/${API_PATH}`
 })
 
 // 取得產品列表 API
-const apiGetProductList = () => baseRequest.get(`/customer/${API_PATH}/products`)
+const apiGetProductList = () => customerRequest.get(`/products`)
 // 取得購物車列表 API
-const apiGetCartList = () => baseRequest.get(`/customer/${API_PATH}/carts`)
+const apiGetCartList = () => customerRequest.get(`/carts`)
 // 加入購物車產品 API
-const apiAddOneProduct = payload => baseRequest.post(`/customer/${API_PATH}/carts`, payload)
+const apiAddOneProduct = payload => customerRequest.post(`/carts`, payload)
 // 刪除購物車產品 API
-const apideleteOneProduct = productId => baseRequest.delete(`/customer/${API_PATH}/carts/${productId}`)
+const apideleteOneProduct = productId => customerRequest.delete(`/carts/${productId}`)
 // 刪除全部購物車產品 API
-const apideleteAllProduct = () => baseRequest.delete(`/customer/${API_PATH}/carts`)
+const apideleteAllProduct = () => customerRequest.delete(`/carts`)
 // 送出訂單 API
-const apiSubmitOrder = payload => baseRequest.post(`/customer/${API_PATH}/orders`, payload)
+const apiSubmitOrder = payload => customerRequest.post(`/orders`, payload)
